19a3e9c66eca30d307563b4660fa554f042c6f73
pigtamer <cygee99@gmail.com>
2018-04-02 15:25:49 +0800

    manip ent plot

--- entropy.py
+++ entropy.py
@@ -6,44 +6,44 @@ import seaborn as sns
 sns.set()
 #%%
 # k = 0
-nx, nt = 1000, 2000
-
-kTv = 0.01; # temp
+nx, nt = 1000, 1000
 
 eps = 1E-3
 
-P = 0.99
+liP = [0.99, -0.99, 0.01]
 
-T_spec = np.linspace(0 + 0.01, 0 + 0.22 , nt)
-P_res = []
-for kTv in T_spec:
-    P_iter = []
-    # print(kTv)
-    while True:
-        dx = 1/nx # each slice
-        # print(dx)
-        nume, deli = 0, 0
+RESes = []
+T_spec = [  np.linspace(0 + 0.01, 0 + 0.28 , nt),
+            np.linspace(0 + 0.01, 0 + 0.28 , nt),
+            np.linspace(0 + 0.01, 0 + 0.28 , nt)
+            ]
+for kP in range(len(liP)):
+    P = liP[kP]
+    P_res = []
+    for kTv in T_spec[kP]:
+        while True:
+            dx = 1/nx # each slice
+            # print(dx)
+            nume, deli = 0, 0
 
-        for k in range(nx):
-            x = k*dx # current loc
-            ak = 1.5*x**2 - 0.5
-            bk = ak / (kTv)
-            nume += ak*np.exp(bk*P)
-            deli += np.exp(bk*P)
-        P_src = P
-        P = nume / deli
+            for k in range(nx):
+                x = k*dx # current loc
+                ak = 1.5*x**2 - 0.5
+                bk = ak / (kTv)
+                nume += ak*np.exp(bk*P)
+                deli += np.exp(bk*P)
+            P_src = P
+            P = nume / deli
+            P_iter.append(P)
+            if np.abs(P_src - P) < eps:
+                P_res.append(P)
+                break
+    RESes.append(P_res)
+    print("Init point: % 5.3f"%P)
+    for k in np.arange(0,len(T_spec[kP])):
+        print("% 5.5f"%T_spec[kP][k], " : ", "% 5.5f"%P_res[k])
 
-        P_iter.append(P)
-        if np.abs(P_src - P) < eps:
-            P_res.append(P)
-            break;
-    # plt.figure()
-    # plt.plot(P_iter)
-    # plt.show()
 #%%
-for k in np.arange(0,len(T_spec)):
-    print("% 5.5f"%T_spec[k], " : ", "% 5.5f"%P_res[k])
-#%%
-plt.figure()
-plt.plot(T_spec, P_res)
+plt.figure(figsize = (10,7))
+plt.plot(T_spec[0], RESes[0], '-', T_spec[1], RESes[1], '-',T_spec[2], RESes[2], '-')
 plt.show()
